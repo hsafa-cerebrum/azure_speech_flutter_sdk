7827e4b4f24b1d6315628345a57716de1cef346e
Hussein <hussein@cerebrum.net>
2025-07-06 10:56:27 +0300

    first commit

---
+++ example/android/app/src/main/kotlin/com/jjordanoc/azure_speech_recognition_null_safety_example/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jjordanoc.azure_speech_recognition_null_safety_example
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
